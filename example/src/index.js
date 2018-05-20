2c25b12640142e95aae0e1b0ed328bb2488d4512
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2018-05-20 16:06:50 +0200

    Init app boilerplate

---
+++ example/src/index.js
@@ -0,0 +1,10 @@
+/* @flow */
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>React Loadable Context example</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["react-hot-loader/patch", __dirname + "/example/src/index.js"],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: __dirname + "/example/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: __dirname + "/example/src/index.html",
+      filename: "./index.html"
+    }),
+
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: __dirname + "/example/dist",
+    hot: true
+  }
+};
